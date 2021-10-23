6d9470fe87b9f8b4b98e93e7bec3c99af81e443f
Anmolroan <90396759+Anmolroan@users.noreply.github.com>
2021-10-23 16:39:36 +0530

    Update index.js

--- assignment/index.js
+++ assignment/index.js
@@ -6,20 +6,8 @@
 // })
 // var QRCode = require('qrcode')
  
-// QRCode.toString('I am a pony!',{type:'terminal'}, function (err, url) {
+// QRCode.toString(',text',{type:'terminal'}, function (err, url) {
 //   console.log(url)
 // })
-const { Command } = require('commander');
-const program = new Command();
-program.version('0.0.1');
-program.argument("<name>","name to change")
-.argument("[number]","number of times to print",1)
-.action(function(name,number){
-    number=Number(number)
-    for (let i =0;i<number;i++){
-        console.log(name)
-    }
-    
-})
-// program.argument()
+
 // node index.js <text_for_qrcode> --output filename.png
